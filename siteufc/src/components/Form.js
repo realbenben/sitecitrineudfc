32f3684bf960da7248f6ab2f82da3ba68a4cfa23
realbenben <b.patou59@gmail.com>
2021-09-09 09:51:28 +0200

    css info-utile done

--- siteufc/src/components/Form.js
+++ siteufc/src/components/Form.js
@@ -17,46 +17,51 @@ const Mailer = () => {
       .catch((err) => console.log(err));
   }
   return (
-    <div className="container-fluid">
-      <h3>formulaire d'inscription</h3>
-      <form className="form-group" onSubmit={sendEmail}>
-        <div className="form-style ">
-          <label for="name_field" className="form-control">
-            Votre nom et prénom
-          </label>
-          <input
-            type="text"
-            className="name_field"
-            name="name"
-            required
-          ></input>
-          <div className="form-style ">
-            <label for="_replyto" className="form-control">
-              Votre email
+    <div>
+      <h3 className="form-title">formulaire d'inscription</h3>
+      <div
+        className="container card"
+        style={{ height: "480px", width: "640px" }}
+      >
+        <form className="form-group" onSubmit={sendEmail}>
+          <div className="form-style">
+            <label for="name_field" className="form-control">
+              Votre nom et prénom
             </label>
             <input
-              type="email"
-              className="_replyto"
-              name="_replyto"
+              type="text"
+              className="name_field"
+              name="name"
               required
             ></input>
+            <div className="form-style ">
+              <label for="_replyto" className="form-control">
+                Votre email
+              </label>
+              <input
+                type="email"
+                className="_replyto"
+                name="_replyto"
+                required
+              ></input>
+            </div>
+            <div className="form-style ">
+              <label for="message" className="form-control">
+                Votre Message
+              </label>
+              <textarea className="textarea_message" name="message"></textarea>
+            </div>
+            <div className="form-style "></div>
+            <button
+              type="submit"
+              className="form-button form-control btn btn-success"
+            >
+              c'est parti !
+            </button>
           </div>
-          <div className="form-style ">
-            <label for="message" className="form-control">
-              Votre Message
-            </label>
-            <textarea className="textarea_message" name="message"></textarea>
-          </div>
-          <div className="form-style "></div>
-          <button
-            type="submit"
-            className="form-button form-control btn btn-success"
-          >
-            c'est parti !
-          </button>
-        </div>
-        <div className="upload"></div>
-      </form>
+          <div className="upload"></div>
+        </form>
+      </div>
     </div>
   );
 };
--- siteufc/src/pages/ContactPage.js
+++ siteufc/src/pages/ContactPage.js
@@ -1,24 +1,33 @@
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
 import Form from "../components/Form";
+import Map from "../components/Map";
 
 const ContactPage = () => {
-    return (
-        <div className='contact'>
-                        
-
-            <Navigation />
-            <div className="main">
-
-            <p>
-                placeholder
-            </p>
-            <Form />
-            </div>
-            <Footer />
-        
+  return (
+    <div className="main">
+      <Navigation />
+      <div className="form-contact row">
+        <div className="col">
+          <Form />
         </div>
-    )
-}
+        <div className="col">
+          <Map />
+          <address>
+            <a href="tel:+648147408">
+              <i class="fas fa-phone"></i> numero direct du Dojo 03 20 45 63 12
+            </a>
+            <br></br>
+            <a href="tel:+648147408">
+              <i class="fas fa-phone"></i>
+              numéro de téléphone pour s'inscrire 06 23 45 63 78
+            </a>
+          </address>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default ContactPage;
